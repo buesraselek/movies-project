770431820c598be3c0da040c8b21b2ce1c45297f
buesraselek <buesra.selek@cloudreach.com>
2022-10-09 23:35:04 +0200

    exercise

---
+++ backend.tf
@@ -0,0 +1,7 @@
+terraform {
+  backend "s3" {
+    bucket = "ta-terraform-tfstates-535268269273"
+    key    = "movies-project/exercise/terraform.tfstates"
+    dynamodb_table = "terraform-lock"
+  }
+}
